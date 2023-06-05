5251b8fa5c8db1b57a8e7e88a4f0a4241fbcde8a
shuang <shuang.lixx@qq.com>
2023-06-05 20:02:07 +0800

    完成copyUI框的开发

--- app/components/DialogBox/DialogItemFooter.tsx
+++ app/components/DialogBox/DialogItemFooter.tsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HiEllipsisVertical } from 'react-icons/hi2'
 import { MdOutlineFileUpload } from 'react-icons/md'
 import { LuThumbsUp, LuThumbsDown } from 'react-icons/lu'
+import { RiFileCopyLine } from 'react-icons/ri'
+import { RiFlag2Line } from 'react-icons/ri'
 import IconItem from '../IconItem'
 import Image from 'next/image'
+import MenuItem from '../Navbar/MenuItem'
 const DialogItemFooter = () => {
+  // 控制copy的显示
+  const [copyIsShow,setCopyIsShow] = useState(false);
+  
   return (
     <div className='flex justify-between items-center p-2 pt-6 pl-16 '>
       <div className='flex justify-center items-center gap-3'>
         <IconItem icon={LuThumbsUp} />
         <IconItem icon={LuThumbsDown} />
         <IconItem icon={MdOutlineFileUpload} />
-        <div className='p-2 border h-[36px] border-[#c4c7c5] rounded-md cursor-pointer hover:bg-[#f9f9f9] flex justify-center items-center active:bg-zinc-300 transition-all'>
+        <div className='p-2 border h-[36px] border-[#c4c7c5] rounded-md cursor-pointer hover:bg-[#f9f9f9] flex justify-center items-center '>
           <Image width={16} height={16} src="/images/Google.svg.png" alt="Google" />
           <span className='text-[14px] ml-2'>Google it</span>
         </div>
       </div>
-      <div>
-        <div className=" flex flex-row justify-center items-center gap-3  ">
+      <div className=' relative'>
+        <div onClick={() => setCopyIsShow(!copyIsShow)} className=" flex flex-row justify-center items-center gap-3  ">
           <HiEllipsisVertical size={48} className=" cursor-pointer hover:bg-zinc-100 hover:rounded-full p-3 active:bg-zinc-300 transition-all" />
         </div>
+        {/* copy */}
+        {copyIsShow && (
+          <div className='border p-1 absolute right-0 bottom-[-100px] w-[220px] bg-white select-none z-20'>
+            <MenuItem onClick={() => { }} label='Copy' icon={RiFileCopyLine} />
+            <MenuItem onClick={() => { }} label='Report legal issue' icon={RiFlag2Line} />
+          </div>
+        )}
+        {/* 透明层 */}
+        {copyIsShow && (
+          <div onClick={() => setCopyIsShow(false)} className='fixed inset-0 bg-black opacity-0 cursor-pointer z-10' />
+        )}
       </div>
     </div>
   )
--- app/components/Navbar/MenuItem.tsx
+++ app/components/Navbar/MenuItem.tsx
@@ -1,24 +1,33 @@
 'use client';
 
+import { IconType } from "react-icons/lib";
+
 interface MenuItemProps {
   onClick: () => void;
-  label: string
+  label: string;
+  icon?:IconType
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({
   onClick,
-  label
+  label,
+  icon:Icon
 }) => {
   return (
     <div
       onClick={onClick}
-      className="
+      className={`
       px-4
       py-3
       hover:bg-neutral-100
       font-semibold
-    "
+       cursor-pointer
+      ${Icon ? 'flex justify-start items-center gap-3' : ''}
+    `}
     >
+      {Icon && (
+        <Icon size={24} className="" />
+      )}
       {label}
     </div>
   );
