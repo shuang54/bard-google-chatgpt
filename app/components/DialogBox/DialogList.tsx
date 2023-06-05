192341c250cb857d8efd45492ffe3668528734f2
shuang <shuang.lixx@qq.com>
2023-06-05 20:41:00 +0800

    读取cookie中的数据，并展示

--- app/components/DialogBox/DialogList.tsx
+++ app/components/DialogBox/DialogList.tsx
@@ -2,10 +2,25 @@
 import React from 'react'
 import DialogItem from './DialogItem'
 import DialogItemBot from './DialogItemBot';
+import { useConversation } from '@/app/hooks/useConversation';
 const DialogList = () => {
+  // 对话记录
+  const { conversation, addMessage, deleteMessage } = useConversation();
+  console.log(conversation)
+  const lists = conversation.map((item)=>{
+    if(item.role == 'user'){
+      return (
+        <DialogItem key={item.id} content='hello' />
+      )
+    }else{
+      return (
+        <DialogItemBot key={item.id} isHeader isFooter content='default' />
+      )
+    }
+  })
   return (
     <div className='p-4 basis-10/12'>
-      <DialogItemBot isHeader isFooter content='default'/>
+      {lists}
     </div>
   )
 }
