9ec2ba481c7f436dfbf7a5b277008271d48d05a9
shuang <shuang.lixx@qq.com>
2023-06-06 13:54:56 +0800

    修改对话列表bug

--- app/components/DialogBox/DialogBox.tsx
+++ app/components/DialogBox/DialogBox.tsx
@@ -5,7 +5,7 @@ import MessageBox from './MessageBox'
 
 const DialogBox = () => {
   return (
-    <div className=' w-full md:w-4/5 sm:p-0 xs:p-0 lg:4/5  xl:w-8/12 overflow-hidden p-10 pt-0 mb-4 '>
+    <div className=' w-full md:w-4/5 sm:p-0 xs:p-0 lg:4/5  xl:w-8/12 overflow-hidden p-10 pt-0 mb-4 h-[100%] '>
       <div className='h-full w-full  bg-[#f3f6fc] rounded-2xl flex flex-col '>
         <DialogList/>
         <MessageBox/>
