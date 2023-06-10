295d22ab0cffaab39169836ccada057fee5dcad6
shuang <shuang.lixx@qq.com>
2023-06-10 10:14:06 +0800

    修改样式

--- app/components/DialogBox/DialogBox.tsx
+++ app/components/DialogBox/DialogBox.tsx
@@ -19,8 +19,8 @@ const DialogBox = () => {
     }
   }
   return (
-    <div className=' w-full md:w-4/5 sm:p-0 xs:p-0 lg:4/5  xl:w-8/12 overflow-hidden p-10 pt-0 mb-4 h-[100%] dark:bg-black'>
-      <div ref={dialogItemBot} className='h-full w-full  bg-[#f3f6fc] rounded-2xl flex flex-col dark:bg-[#222327]'>
+    <div className=' w-full md:w-4/5 sm:p-0 xs:p-0 lg:4/5  xl:w-8/12 overflow-hidden p-10 pt-0 mb-4 h-[100%] dark:bg-black bg-[#f3f6fc] rounded-2xl'>
+      <div ref={dialogItemBot} className='h-full w-full   flex flex-col dark:bg-[#222327] '>
         <DialogList/>
         <MessageBox handleScrollDown={handleScrollDown }/>
       </div>
